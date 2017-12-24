e435b31a45f6000d0d32c20f70f88afe7f26d74a
white-veil <brianrotych@gmail.com>
2017-12-24 23:56:52 +0300

    (feat) fix few call to super method

--- viewer/models/kind.py
+++ viewer/models/kind.py
@@ -3,17 +3,16 @@ from viewer.models.common import BaseModel
 
 class KindModel(BaseModel):
     def __init__(self, kind, namespace='default'):
-        self.__kind__ = kind
-        self.__namespace = namespace
+        super().__init__(kind, namespace)
 
     @classmethod
     def get_all_available_kinds(cls):
-        """gets all kinds of entites available
+        """gets all kinds of entities available
 
         Returns
         --------------
         list(str)
-            containing names of available enity kinds in the datastore
+            containing names of available entity kinds in the datastore
         """
-        query = cls.client().query(kind='__kind__')
+        query = cls.client().query(**dict(kind='__kind__'))
         return [entity.key.id_or_name for entity in query.fetch()]
