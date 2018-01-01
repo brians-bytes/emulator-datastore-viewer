ccea460062d7df1dc20317a97ce0340af6d87b91
white-veil <brianrotych@gmail.com>
2018-01-01 21:58:01 +0300

    (feat) add retrieve available kinds using namespaces

--- viewer/api/datastore/endpoints/kinds.py
+++ viewer/api/datastore/endpoints/kinds.py
@@ -1,10 +1,14 @@
-from flask_restplus import Resource
-
+from flask_restplus import Resource, reqparse
 from viewer.api.datastore.serializers import kind_collection
 from viewer.api.restplus import api
 from viewer.models.kind import KindModel
 
 ns = api.namespace('kinds', description='Kinds Endpoints')
+
+parser = reqparse.RequestParser()
+parser.add_argument(
+    'namespace', type=str, help='limit the search to this namespace'
+)
 
 
 @ns.route('')
@@ -14,12 +18,17 @@ class NamespaceCollection(Resource):
     })
     @api.marshal_with(kind_collection)
     def get(self):
-        current_kinds = KindModel.get_available_namespaced_entity_kinds()
+        args = parser.parse_args()
+        namespace = args['namespace']
+
+        available_kinds = KindModel.get_available_namespaced_entity_kinds(
+            namespace
+        )
         return {
             'items': [
                 {
                     'name': kind['kind'],
                     'namespace': kind['namespace'] or 'default',
-                } for kind in current_kinds
+                } for kind in available_kinds
             ],
         }
