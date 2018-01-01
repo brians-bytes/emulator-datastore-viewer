67724ff11636e57b3146bc28960bd24f9f321f17
white-veil <brianrotych@gmail.com>
2018-01-01 23:33:07 +0300

    (test) move test fixture function to utils folder

--- tests/integration_tests/endpoint_tests/test_namespace_endpoint.py
+++ tests/integration_tests/endpoint_tests/test_namespace_endpoint.py
@@ -1,26 +1,7 @@
 import json
 
 from tests.base_test import BaseAPITest
-from tests.test_factories import KindFactory
-
-
-def create_mocked_kinds(batch=10):
-    """creates and test entities for API test
-
-    Parameters
-    ----------
-    batch : int
-        number of enities to create, defalut to 10
-
-    Returns
-    -------
-    list of KindModel
-    """
-    kind_list = KindFactory.create_batch(batch)
-    for kind in kind_list:
-        kind.put()
-
-    return kind_list
+from tests.test_utils import create_mocked_kinds
 
 
 class KindEnpointIntegrationTests(BaseAPITest):
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import requests
+from tests.test_factories import KindFactory
 
 logger = logging.getLogger(__name__)
 
@@ -15,3 +16,22 @@ def reset_datastore_emulator():
     if r.status_code != 200:
         logger.warn(r.content)
         raise RuntimeError('Cannot reset datastore emulator')
+
+
+def create_mocked_kinds(batch=10):
+    """creates and test entities for API test
+
+    Parameters
+    ----------
+    batch : int
+        number of enities to create, defalut to 10
+
+    Returns
+    -------
+    list of KindModel
+    """
+    kind_list = KindFactory.create_batch(batch)
+    for kind in kind_list:
+        kind.put()
+
+    return kind_list
